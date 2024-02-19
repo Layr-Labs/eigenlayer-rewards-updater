651ca83a4fb82b7f076ac8eaea1f1cc1b3ba1fd2
gpsanant <gpsanant@gmail.com>
2024-02-18 17:00:40 -0800

    remove query

--- calculator/sql.go
+++ calculator/sql.go
@@ -1,13 +1,5 @@
 package calculator
 
-// gets all range payments that overlap with the given range ($1, $2)
-var overlappingRangePaymentsQuery string = `
-	SELECT range_payment_avs, range_payment_strategy, range_payment_token, range_payment_amount, range_payment_start_range_timestamp, range_payment_end_range_timestamp
-	FROM %s.range_payment_created
-	WHERE range_payment_start_range_timestamp < $2 AND range_payment_end_range_timestamp > $1
-	LIMIT 1;
-`
-
 var claimersAtTimestampQuery string = `
 	SELECT DISTINCT ON (account) account, claimer
 	FROM %s.claimer_set
