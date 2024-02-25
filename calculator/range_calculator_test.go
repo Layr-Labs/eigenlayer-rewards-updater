0bb0a1959999486cebcaaf9ce1b46ad982c66228
gpsanant <gpsanant@gmail.com>
2024-02-25 15:58:44 -0800

    fix copy checking

--- calculator/range_calculator_test.go
+++ calculator/range_calculator_test.go
@@ -84,7 +84,7 @@ func TestCalculateDistributionToOperatorForInterval(t *testing.T) {
 		diffDistribution := distribution.NewDistribution()
 		diffDistribution = CalculateDistributionToOperatorForInterval(context.Background(), diffDistribution, 0, operatorSet, STETH_ADDRESS, normalPaymentToDistributePerInterval)
 
-		assert.Equal(t, normalPaymentToDistributePerInterval, diffDistribution.Get(utils.SelfDelegatedOperator.Claimer, STETH_ADDRESS))
+		assert.Equal(t, normalPaymentToDistributePerInterval, diffDistribution.Get(operatorSet.Operators[0].Claimer, STETH_ADDRESS))
 	})
 
 	t.Run("test CalculateDistributionToOperatorForInterval for single operator with 2 outside stakers", func(t *testing.T) {
@@ -97,9 +97,9 @@ func TestCalculateDistributionToOperatorForInterval(t *testing.T) {
 
 		diffDistribution = CalculateDistributionToOperatorForInterval(context.Background(), diffDistribution, 0, operatorSet, STETH_ADDRESS, normalPaymentToDistributePerInterval)
 
-		assert.Equal(t, big.NewInt(100000000000), diffDistribution.Get(utils.OperatorWith2OutsideStakers.Claimer, STETH_ADDRESS))
-		assert.Equal(t, big.NewInt(450000000000), diffDistribution.Get(utils.OperatorWith2OutsideStakers.Stakers[1].Claimer, STETH_ADDRESS))
-		assert.Equal(t, big.NewInt(450000000000), diffDistribution.Get(utils.OperatorWith2OutsideStakers.Stakers[2].Claimer, STETH_ADDRESS))
+		assert.Equal(t, big.NewInt(100000000000), diffDistribution.Get(operatorSet.Operators[0].Claimer, STETH_ADDRESS))
+		assert.Equal(t, big.NewInt(450000000000), diffDistribution.Get(operatorSet.Operators[0].Stakers[1].Claimer, STETH_ADDRESS))
+		assert.Equal(t, big.NewInt(450000000000), diffDistribution.Get(operatorSet.Operators[0].Stakers[2].Claimer, STETH_ADDRESS))
 	})
 
 	t.Run("test CalculateDistributionToOperatorForInterval for single operator with 1 outside staker", func(t *testing.T) {
@@ -112,8 +112,8 @@ func TestCalculateDistributionToOperatorForInterval(t *testing.T) {
 
 		diffDistribution = CalculateDistributionToOperatorForInterval(context.Background(), diffDistribution, 0, operatorSet, STETH_ADDRESS, normalPaymentToDistributePerInterval)
 
-		assert.Equal(t, big.NewInt(550000000000), diffDistribution.Get(utils.OperatorWith1OutsideStaker.Claimer, STETH_ADDRESS))
-		assert.Equal(t, big.NewInt(450000000000), diffDistribution.Get(utils.OperatorWith1OutsideStaker.Stakers[1].Claimer, STETH_ADDRESS))
+		assert.Equal(t, big.NewInt(550000000000), diffDistribution.Get(operatorSet.Operators[0].Claimer, STETH_ADDRESS))
+		assert.Equal(t, big.NewInt(450000000000), diffDistribution.Get(operatorSet.Operators[0].Stakers[1].Claimer, STETH_ADDRESS))
 	})
 
 	t.Run("test CalculateDistributionToOperatorForInterval for three operators equal split", func(t *testing.T) {
@@ -128,12 +128,12 @@ func TestCalculateDistributionToOperatorForInterval(t *testing.T) {
 		diffDistribution = CalculateDistributionToOperatorForInterval(context.Background(), diffDistribution, 1, operatorSet, STETH_ADDRESS, normalPaymentToDistributePerInterval)
 		diffDistribution = CalculateDistributionToOperatorForInterval(context.Background(), diffDistribution, 2, operatorSet, STETH_ADDRESS, normalPaymentToDistributePerInterval)
 
-		assert.Equal(t, big.NewInt(333333333333), diffDistribution.Get(utils.SelfDelegatedOperator.Claimer, STETH_ADDRESS))
-		assert.Equal(t, big.NewInt(33333333333), diffDistribution.Get(utils.OperatorWith2OutsideStakers.Claimer, STETH_ADDRESS))
-		assert.Equal(t, big.NewInt(150000000000), diffDistribution.Get(utils.OperatorWith2OutsideStakers.Stakers[1].Claimer, STETH_ADDRESS))
-		assert.Equal(t, big.NewInt(150000000000), diffDistribution.Get(utils.OperatorWith2OutsideStakers.Stakers[2].Claimer, STETH_ADDRESS))
-		assert.Equal(t, big.NewInt(183333333333), diffDistribution.Get(utils.OperatorWith1OutsideStaker.Claimer, STETH_ADDRESS))
-		assert.Equal(t, big.NewInt(150000000000), diffDistribution.Get(utils.OperatorWith1OutsideStaker.Stakers[1].Claimer, STETH_ADDRESS))
+		assert.Equal(t, big.NewInt(333333333333), diffDistribution.Get(operatorSet.Operators[0].Claimer, STETH_ADDRESS))
+		assert.Equal(t, big.NewInt(33333333333), diffDistribution.Get(operatorSet.Operators[1].Claimer, STETH_ADDRESS))
+		assert.Equal(t, big.NewInt(150000000000), diffDistribution.Get(operatorSet.Operators[1].Stakers[1].Claimer, STETH_ADDRESS))
+		assert.Equal(t, big.NewInt(150000000000), diffDistribution.Get(operatorSet.Operators[1].Stakers[2].Claimer, STETH_ADDRESS))
+		assert.Equal(t, big.NewInt(183333333333), diffDistribution.Get(operatorSet.Operators[2].Claimer, STETH_ADDRESS))
+		assert.Equal(t, big.NewInt(150000000000), diffDistribution.Get(operatorSet.Operators[2].Stakers[1].Claimer, STETH_ADDRESS))
 	})
 
 	t.Run("test CalculateDistributionToOperatorForInterval for three operators 1/10 3/10 6/10", func(t *testing.T) {
@@ -156,12 +156,12 @@ func TestCalculateDistributionToOperatorForInterval(t *testing.T) {
 		diffDistribution = CalculateDistributionToOperatorForInterval(context.Background(), diffDistribution, 1, operatorSet, STETH_ADDRESS, normalPaymentToDistributePerInterval)
 		diffDistribution = CalculateDistributionToOperatorForInterval(context.Background(), diffDistribution, 2, operatorSet, STETH_ADDRESS, normalPaymentToDistributePerInterval)
 
-		assert.Equal(t, big.NewInt(100000000000), diffDistribution.Get(utils.SelfDelegatedOperator.Claimer, STETH_ADDRESS))
-		assert.Equal(t, big.NewInt(30000000000), diffDistribution.Get(utils.OperatorWith2OutsideStakers.Claimer, STETH_ADDRESS))
-		assert.Equal(t, big.NewInt(180000000000), diffDistribution.Get(utils.OperatorWith2OutsideStakers.Stakers[1].Claimer, STETH_ADDRESS))
-		assert.Equal(t, big.NewInt(90000000000), diffDistribution.Get(utils.OperatorWith2OutsideStakers.Stakers[2].Claimer, STETH_ADDRESS))
-		assert.Equal(t, big.NewInt(420000000000), diffDistribution.Get(utils.OperatorWith1OutsideStaker.Claimer, STETH_ADDRESS))
-		assert.Equal(t, big.NewInt(180000000000), diffDistribution.Get(utils.OperatorWith1OutsideStaker.Stakers[1].Claimer, STETH_ADDRESS))
+		assert.Equal(t, big.NewInt(100000000000), diffDistribution.Get(operatorSet.Operators[0].Claimer, STETH_ADDRESS))
+		assert.Equal(t, big.NewInt(30000000000), diffDistribution.Get(operatorSet.Operators[1].Claimer, STETH_ADDRESS))
+		assert.Equal(t, big.NewInt(180000000000), diffDistribution.Get(operatorSet.Operators[1].Stakers[1].Claimer, STETH_ADDRESS))
+		assert.Equal(t, big.NewInt(90000000000), diffDistribution.Get(operatorSet.Operators[1].Stakers[2].Claimer, STETH_ADDRESS))
+		assert.Equal(t, big.NewInt(420000000000), diffDistribution.Get(operatorSet.Operators[2].Claimer, STETH_ADDRESS))
+		assert.Equal(t, big.NewInt(180000000000), diffDistribution.Get(operatorSet.Operators[2].Stakers[1].Claimer, STETH_ADDRESS))
 	})
 
 	t.Run("test CalculateDistributionToOperatorForInterval for two operators with tiny payment", func(t *testing.T) {
@@ -176,10 +176,10 @@ func TestCalculateDistributionToOperatorForInterval(t *testing.T) {
 		diffDistribution = CalculateDistributionToOperatorForInterval(context.Background(), diffDistribution, 0, operatorSet, STETH_ADDRESS, tinyPaymentToDistributePerInterval)
 		diffDistribution = CalculateDistributionToOperatorForInterval(context.Background(), diffDistribution, 1, operatorSet, STETH_ADDRESS, tinyPaymentToDistributePerInterval)
 
-		assert.Equal(t, big.NewInt(31), diffDistribution.Get(utils.SelfDelegatedOperator.Claimer, STETH_ADDRESS))
-		assert.Equal(t, big.NewInt(1), diffDistribution.Get(utils.OperatorWith2OutsideStakers.Claimer, STETH_ADDRESS))
-		assert.Equal(t, big.NewInt(2), diffDistribution.Get(utils.OperatorWith2OutsideStakers.Stakers[1].Claimer, STETH_ADDRESS))
-		assert.Equal(t, big.NewInt(14), diffDistribution.Get(utils.OperatorWith2OutsideStakers.Stakers[2].Claimer, STETH_ADDRESS))
+		assert.Equal(t, big.NewInt(31), diffDistribution.Get(operatorSet.Operators[0].Claimer, STETH_ADDRESS))
+		assert.Equal(t, big.NewInt(1), diffDistribution.Get(operatorSet.Operators[1].Claimer, STETH_ADDRESS))
+		assert.Equal(t, big.NewInt(2), diffDistribution.Get(operatorSet.Operators[1].Stakers[1].Claimer, STETH_ADDRESS))
+		assert.Equal(t, big.NewInt(14), diffDistribution.Get(operatorSet.Operators[1].Stakers[2].Claimer, STETH_ADDRESS))
 	})
 
 	t.Run("test CalculateDistributionToOperatorForInterval for two operators with tiny payment round down to 0", func(t *testing.T) {
@@ -195,10 +195,39 @@ func TestCalculateDistributionToOperatorForInterval(t *testing.T) {
 		diffDistribution = CalculateDistributionToOperatorForInterval(context.Background(), diffDistribution, 0, operatorSet, STETH_ADDRESS, tinyPaymentToDistributePerInterval)
 		diffDistribution = CalculateDistributionToOperatorForInterval(context.Background(), diffDistribution, 1, operatorSet, STETH_ADDRESS, tinyPaymentToDistributePerInterval)
 
-		assert.Equal(t, big.NewInt(41), diffDistribution.Get(utils.SelfDelegatedOperator.Claimer, STETH_ADDRESS))
-		assert.Equal(t, big.NewInt(0), diffDistribution.Get(utils.OperatorWith2OutsideStakers.Claimer, STETH_ADDRESS))
-		assert.Equal(t, big.NewInt(4), diffDistribution.Get(utils.OperatorWith2OutsideStakers.Stakers[1].Claimer, STETH_ADDRESS))
-		assert.Equal(t, big.NewInt(4), diffDistribution.Get(utils.OperatorWith2OutsideStakers.Stakers[2].Claimer, STETH_ADDRESS))
+		assert.Equal(t, big.NewInt(41), diffDistribution.Get(operatorSet.Operators[0].Claimer, STETH_ADDRESS))
+		assert.Equal(t, big.NewInt(0), diffDistribution.Get(operatorSet.Operators[1].Claimer, STETH_ADDRESS))
+		assert.Equal(t, big.NewInt(4), diffDistribution.Get(operatorSet.Operators[1].Stakers[1].Claimer, STETH_ADDRESS))
+		assert.Equal(t, big.NewInt(4), diffDistribution.Get(operatorSet.Operators[1].Stakers[2].Claimer, STETH_ADDRESS))
+	})
+
+	t.Run("test CalculateDistributionToOperatorForInterval for three operators 1/10 3/10 6/10 with randomized claimers", func(t *testing.T) {
+		operatorSet := &common.OperatorSet{
+			Operators: []*common.Operator{utils.GetSelfDelegatedOperator(), utils.GetOperatorWith2OutsideStakers(), utils.GetOperatorWith1OutsideStaker()},
+		}
+		operatorSet.ModifyStrategyShares(utils.TEST_OPERATOR_ADDRESS_1, utils.TEST_OPERATOR_ADDRESS_1, big.NewInt(1e17))
+
+		operatorSet.ModifyStrategyShares(utils.TEST_OPERATOR_ADDRESS_2, utils.TEST_STAKER_ADDRESS_1, big.NewInt(2e17))
+		operatorSet.ModifyStrategyShares(utils.TEST_OPERATOR_ADDRESS_2, utils.TEST_STAKER_ADDRESS_2, big.NewInt(1e17))
+
+		operatorSet.ModifyStrategyShares(utils.TEST_OPERATOR_ADDRESS_3, utils.TEST_OPERATOR_ADDRESS_3, big.NewInt(4e17))
+		operatorSet.ModifyStrategyShares(utils.TEST_OPERATOR_ADDRESS_3, utils.TEST_STAKER_ADDRESS_3, big.NewInt(2e17))
+
+		operatorSet.FillTotals()
+		operatorSet.RandomizeClaimers()
+
+		diffDistribution := distribution.NewDistribution()
+
+		diffDistribution = CalculateDistributionToOperatorForInterval(context.Background(), diffDistribution, 0, operatorSet, STETH_ADDRESS, normalPaymentToDistributePerInterval)
+		diffDistribution = CalculateDistributionToOperatorForInterval(context.Background(), diffDistribution, 1, operatorSet, STETH_ADDRESS, normalPaymentToDistributePerInterval)
+		diffDistribution = CalculateDistributionToOperatorForInterval(context.Background(), diffDistribution, 2, operatorSet, STETH_ADDRESS, normalPaymentToDistributePerInterval)
+
+		assert.Equal(t, big.NewInt(100000000000), diffDistribution.Get(operatorSet.Operators[0].Claimer, STETH_ADDRESS))
+		assert.Equal(t, big.NewInt(30000000000), diffDistribution.Get(operatorSet.Operators[1].Claimer, STETH_ADDRESS))
+		assert.Equal(t, big.NewInt(180000000000), diffDistribution.Get(operatorSet.Operators[1].Stakers[1].Claimer, STETH_ADDRESS))
+		assert.Equal(t, big.NewInt(90000000000), diffDistribution.Get(operatorSet.Operators[1].Stakers[2].Claimer, STETH_ADDRESS))
+		assert.Equal(t, big.NewInt(420000000000), diffDistribution.Get(operatorSet.Operators[2].Claimer, STETH_ADDRESS))
+		assert.Equal(t, big.NewInt(180000000000), diffDistribution.Get(operatorSet.Operators[2].Stakers[1].Claimer, STETH_ADDRESS))
 	})
 
 }
