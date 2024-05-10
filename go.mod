aa2d9d007abfcd2cf0e2fb326a810a847471785f
Sean McGary <sean@seanmcgary.com>
2024-05-10 13:25:47 -0500

    Adding ProofStore

--- go.mod
+++ go.mod
@@ -1,10 +1,12 @@
 module github.com/Layr-Labs/eigenlayer-payment-updater
 
-go 1.22.2
+go 1.22
+
+toolchain go1.22.3
 
 require (
 	github.com/DATA-DOG/go-sqlmock v1.4.1
-	github.com/Layr-Labs/eigenlayer-payment-proofs v0.0.0-20240501211720-3876608382bd
+	github.com/Layr-Labs/eigenlayer-payment-proofs v0.0.0-20240510171113-d182b4b35eb3
 	github.com/ethereum/go-ethereum v1.14.0
 	github.com/pkg/errors v0.9.1
 	github.com/rs/zerolog v1.31.0
@@ -26,6 +28,7 @@ require (
 	github.com/bits-and-blooms/bitset v1.13.0 // indirect
 	github.com/btcsuite/btcd/btcec/v2 v2.3.2 // indirect
 	github.com/buger/jsonparser v1.1.1 // indirect
+	github.com/chigopher/pathlib v0.19.1 // indirect
 	github.com/consensys/bavard v0.1.13 // indirect
 	github.com/consensys/gnark-crypto v0.12.1 // indirect
 	github.com/crate-crypto/go-kzg-4844 v1.0.0 // indirect
@@ -39,14 +42,18 @@ require (
 	github.com/gorilla/websocket v1.4.2 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/holiman/uint256 v1.2.4 // indirect
+	github.com/huandu/xstrings v1.4.0 // indirect
+	github.com/iancoleman/strcase v0.2.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/jedib0t/go-pretty/v6 v6.2.7 // indirect
+	github.com/jinzhu/copier v0.3.5 // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mattn/go-runewidth v0.0.13 // indirect
+	github.com/mitchellh/go-homedir v1.1.0 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/mmcloughlin/addchain v0.4.0 // indirect
 	github.com/pelletier/go-toml/v2 v2.1.0 // indirect
@@ -64,6 +71,7 @@ require (
 	github.com/tklauser/go-sysconf v0.3.12 // indirect
 	github.com/tklauser/numcpus v0.6.1 // indirect
 	github.com/uber-go/tally v3.3.17+incompatible // indirect
+	github.com/vektra/mockery/v2 v2.43.0 // indirect
 	github.com/wk8/go-ordered-map/v2 v2.1.8 // indirect
 	github.com/xwb1989/sqlparser v0.0.0-20180606152119-120387863bf2 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
@@ -72,9 +80,11 @@ require (
 	golang.org/x/mod v0.17.0 // indirect
 	golang.org/x/sync v0.7.0 // indirect
 	golang.org/x/sys v0.19.0 // indirect
+	golang.org/x/term v0.19.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 	golang.org/x/tools v0.20.0 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	rsc.io/tmplfunc v0.0.3 // indirect
 )
---
+++ pkg/proofStore/http.go
@@ -0,0 +1,205 @@
+package proofStore
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/Layr-Labs/eigenlayer-payment-proofs/pkg/distribution"
+	"github.com/Layr-Labs/eigenlayer-payment-updater/pkg/utils"
+	"go.uber.org/zap"
+	"io"
+	"net/http"
+	"strings"
+	"time"
+)
+
+type HTTPProofStore struct {
+	BaseUrl     string
+	Environment string
+	Network     string
+
+	RecentPayments []SubmittedPayment
+	ActivePayment  *SubmittedPayment
+	Proofs         map[string]*PaymentProofData
+	logger         *zap.Logger
+}
+
+func NewHTTPProofStore(baseUrl string, env string, network string, l *zap.Logger) *HTTPProofStore {
+	return &HTTPProofStore{
+		BaseUrl:     baseUrl,
+		Environment: env,
+		Network:     network,
+		Proofs:      make(map[string]*PaymentProofData),
+		logger:      l,
+	}
+}
+
+func (h *HTTPProofStore) GetProofForActivePayment() (*PaymentProofData, error) {
+	var proof *PaymentProofData
+	var err error
+
+	if h.ActivePayment == nil {
+		err := h.refreshSnapshotData()
+		if err != nil {
+			return nil, err
+		}
+
+		// If activePayment is still nil, then we have no active payment
+		if h.ActivePayment == nil {
+			return nil, fmt.Errorf("no active payment found")
+		}
+	}
+	proof, ok := h.Proofs[h.ActivePayment.GetPaymentDate()]
+	if !ok {
+		h.logger.Sugar().Infof("No proof found for date %s. Fetching proof", h.ActivePayment.GetPaymentDate())
+		proof, err = h.fetchClaimAmountsForDate(h.ActivePayment)
+		if err != nil {
+			return nil, fmt.Errorf("failed to fetch claim amounts for date %s: %s", h.ActivePayment.GetPaymentDate(), err)
+		}
+	}
+
+	return proof, nil
+}
+
+// refreshSnapshotData:
+//   - fetches the list of submitted payments from the S3 bucket
+//   - For the active payment, fetches the claim amounts
+func (h *HTTPProofStore) refreshSnapshotData() error {
+	err := h.fetchSubmittedPayments()
+	if err != nil {
+		return err
+	}
+
+	if h.ActivePayment == nil {
+		return fmt.Errorf("no active payment found")
+	}
+
+	h.logger.Sugar().Debug("Fetching active payment")
+	_, err = h.fetchClaimAmountsForDate(h.ActivePayment)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// fetchSubmittedPayments: fetches the list of on-chain submitted payments from the S3 bucket
+func (h *HTTPProofStore) fetchSubmittedPayments() error {
+	// <baseurl>/<env>/<network>/submitted-payments.json
+	u := fmt.Sprintf("%s/%s/%s/submitted-payments.json",
+		h.BaseUrl,
+		h.Environment,
+		h.Network,
+	)
+	h.logger.Sugar().Debugf("Getting submitted payments from %s", u)
+	resp, err := http.Get(u)
+
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	rawBody, err := io.ReadAll(resp.Body)
+
+	if err != nil {
+		return fmt.Errorf("failed to read response body: %s", err)
+	}
+
+	if resp.StatusCode >= 400 {
+		return fmt.Errorf("failed to fetch submitted-payments. got status code %d", resp.StatusCode)
+	}
+
+	submittedPayments := make([]SubmittedPayment, 0)
+	err = json.Unmarshal(rawBody, &submittedPayments)
+
+	h.RecentPayments = submittedPayments
+
+	if len(submittedPayments) == 0 {
+		return nil
+	}
+
+	// If the latest payment has reached its activation date, it is the active payment and can be claimed against
+	if submittedPayments[0].ActivatedAt.Before(time.Now()) {
+		h.ActivePayment = &submittedPayments[0]
+		return nil
+	} else if len(submittedPayments) > 1 {
+		// Otherwise, the active payment is the one before the latest payment
+		h.ActivePayment = &submittedPayments[1]
+	}
+	// Else, we'll leave the active payment as nil
+	return err
+}
+
+// fetchClaimAmountsForDate: fetches the claim amounts for a given payment date
+func (h *HTTPProofStore) fetchClaimAmountsForDate(submittedPayment *SubmittedPayment) (*PaymentProofData, error) {
+	paymentDate := submittedPayment.GetPaymentDate()
+
+	// <baseurl>/<env>/<network>/<snapshot_date>/claim_amounts.json
+	u := fmt.Sprintf("%s/%s/%s/%s/claim-amounts.json",
+		h.BaseUrl,
+		h.Environment,
+		h.Network,
+		paymentDate,
+	)
+	h.logger.Sugar().Debugf("Getting claim amounts for %s from %s", paymentDate, u)
+	resp, err := http.Get(u)
+
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	rawBody, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	if resp.StatusCode >= 400 {
+		return nil, fmt.Errorf("failed to fetch snapshot for %s: %s. Got status code %d", paymentDate, string(rawBody), resp.StatusCode)
+	}
+
+	proof, err := h.processSnapshotFromRawBody(rawBody)
+	if err != nil {
+		h.logger.Sugar().Errorf("Failed to process snapshot from raw body: %s", err)
+		return nil, err
+	}
+	h.Proofs[paymentDate] = proof
+	h.logger.Sugar().Infof("Added proof %s for date %s to cache", proof.Hash, paymentDate)
+	return proof, nil
+}
+
+func (h *HTTPProofStore) processSnapshotFromRawBody(rawBody []byte) (*PaymentProofData, error) {
+	strLines := strings.Split(string(rawBody), "\n")
+
+	distro := distribution.NewDistribution()
+	lines := []*distribution.EarnerLine{}
+	for _, line := range strLines {
+		if line == "" {
+			continue
+		}
+		earner := &distribution.EarnerLine{}
+		if err := json.Unmarshal([]byte(line), earner); err != nil {
+			h.logger.Sugar().Errorf("Failed to unmarshal line: %s", line)
+			return nil, err
+		}
+		lines = append(lines, earner)
+	}
+	if err := distro.LoadLines(lines); err != nil {
+
+		h.logger.Sugar().Errorf("Failed to load lines: %s\n", err)
+		return nil, err
+	}
+
+	accountTree, tokenTree, err := distro.Merklize()
+	if err != nil {
+		return nil, err
+	}
+
+	proof := &PaymentProofData{
+		Distribution: distro,
+		AccountTree:  accountTree,
+		TokenTree:    tokenTree,
+		Hash:         utils.ConvertBytesToString(accountTree.Root()),
+	}
+
+	return proof, nil
+}
