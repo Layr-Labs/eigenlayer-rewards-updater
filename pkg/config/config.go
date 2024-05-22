ca4f55053654fc9d2c922598e651110bbafab6b2
Sean McGary <sean@seanmcgary.com>
2024-05-22 11:10:59 -0500

    Add config for signing backend

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -21,14 +21,22 @@ var (
 	Environment_PROD     Environment = 3
 )
 
+type SigningBackendKind int
+
+var (
+	SigningBackendKind_PrivateKey SigningBackendKind = 1
+	SigningBackendKind_Ledger     SigningBackendKind = 2
+)
+
 type UpdaterConfig struct {
 	GlobalConfig
-	Environment               Environment `mapstructure:"environment"`
-	Network                   string      `mapstructure:"network"`
-	RPCUrl                    string      `mapstructure:"rpc_url"`
-	PrivateKey                string      `mapstructure:"private_key"`
-	PaymentCoordinatorAddress string      `mapstructure:"payment_coordinator_address"`
-	ProofStoreBaseUrl         string      `mapstructure:"proof_store_base_url"`
+	Environment               Environment        `mapstructure:"environment"`
+	Network                   string             `mapstructure:"network"`
+	RPCUrl                    string             `mapstructure:"rpc_url"`
+	SigningBackend            SigningBackendKind `mapstructure:"signing_backend"`
+	PrivateKey                string             `mapstructure:"private_key"`
+	PaymentCoordinatorAddress string             `mapstructure:"payment_coordinator_address"`
+	ProofStoreBaseUrl         string             `mapstructure:"proof_store_base_url"`
 }
 
 type DistributionConfig struct {
@@ -88,6 +96,27 @@ func StringEnvironmentFromEnum(env Environment) (string, error) {
 	return "", errors.New(fmt.Sprintf("String env not found for '%d'", env))
 }
 
+func parseSigningBackend(signingBackend string) SigningBackendKind {
+	switch signingBackend {
+	case "private_key":
+		return SigningBackendKind_PrivateKey
+	case "ledger":
+		return SigningBackendKind_Ledger
+	default:
+		return SigningBackendKind_PrivateKey
+	}
+}
+
+func StringSigningBackendFromEnum(signingBackend SigningBackendKind) (string, error) {
+	switch signingBackend {
+	case SigningBackendKind_PrivateKey:
+		return "private_key", nil
+	case SigningBackendKind_Ledger:
+		return "ledger", nil
+	}
+	return "", errors.New(fmt.Sprintf("String signing backend not found for '%d'", signingBackend))
+}
+
 // NewUpdaterConfig reads config values from viper and returns
 // them in a struct
 func NewUpdaterConfig() *UpdaterConfig {
@@ -99,6 +128,7 @@ func NewUpdaterConfig() *UpdaterConfig {
 		Environment:               parseEnvironment(viper.GetString("environment")),
 		Network:                   viper.GetString("network"),
 		RPCUrl:                    viper.GetString("rpc_url"),
+		SigningBackend:            parseSigningBackend(viper.GetString("signing_backend")),
 		PrivateKey:                viper.GetString("private_key"),
 		PaymentCoordinatorAddress: viper.GetString("payment_coordinator_address"),
 		ProofStoreBaseUrl:         viper.GetString("proof_store_base_url"),
