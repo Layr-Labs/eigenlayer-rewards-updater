71ebabd320ea66f57e4926d7f05c153c9109a284
Sean McGary <sean@seanmcgary.com>
2024-06-03 14:55:47 -0500

    Disable datadog trace startup logs

--- pkg/tracer/tracer.go
+++ pkg/tracer/tracer.go
@@ -2,9 +2,11 @@ package tracer
 
 import (
 	ddTracer "gopkg.in/DataDog/dd-trace-go.v1/ddtrace/tracer"
+	"os"
 )
 
 func StartTracer() {
+	os.Setenv("DD_TRACE_STARTUP_LOGS", "false")
 	ddTracer.Start(
 		ddTracer.WithGlobalServiceName(true),
 		ddTracer.WithDebugMode(false),
