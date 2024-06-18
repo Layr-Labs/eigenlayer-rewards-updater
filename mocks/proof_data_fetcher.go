02c3dad628260aba8e04fe34b434293a1ba90932
Sean McGary <sean@seanmcgary.com>
2024-06-18 14:00:26 -0500

    Update mocks;

--- mocks/proof_data_fetcher.go
+++ mocks/proof_data_fetcher.go
@@ -74,6 +74,36 @@ func (_m *ProofDataFetcher) FetchLatestSnapshot(ctx context.Context) (*proofData
 	return r0, r1
 }
 
+// FetchPostedRewards provides a mock function with given fields: ctx
+func (_m *ProofDataFetcher) FetchPostedRewards(ctx context.Context) ([]*proofDataFetcher.SubmittedRewardRoot, error) {
+	ret := _m.Called(ctx)
+
+	if len(ret) == 0 {
+		panic("no return value specified for FetchPostedRewards")
+	}
+
+	var r0 []*proofDataFetcher.SubmittedRewardRoot
+	var r1 error
+	if rf, ok := ret.Get(0).(func(context.Context) ([]*proofDataFetcher.SubmittedRewardRoot, error)); ok {
+		return rf(ctx)
+	}
+	if rf, ok := ret.Get(0).(func(context.Context) []*proofDataFetcher.SubmittedRewardRoot); ok {
+		r0 = rf(ctx)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).([]*proofDataFetcher.SubmittedRewardRoot)
+		}
+	}
+
+	if rf, ok := ret.Get(1).(func(context.Context) error); ok {
+		r1 = rf(ctx)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
 // FetchRecentSnapshotList provides a mock function with given fields: ctx
 func (_m *ProofDataFetcher) FetchRecentSnapshotList(ctx context.Context) ([]*proofDataFetcher.Snapshot, error) {
 	ret := _m.Called(ctx)
